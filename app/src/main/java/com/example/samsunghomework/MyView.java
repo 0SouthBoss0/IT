c254949282c33a66e0d21200ddb349520213efd8
0SouthBoss0 <mishumpel@mail.ru>
2019-10-19 15:37:05 +0300

    1.7.6.3
    kvadrat

--- app/src/main/java/com/example/samsunghomework/MyView.java
+++ app/src/main/java/com/example/samsunghomework/MyView.java
@@ -25,14 +25,14 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        paint.setColor(Color.YELLOW);
+        paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(300, 300, 200, paint);
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(20);
-        canvas.drawCircle(300, 300, 200, paint);
-        }
+        canvas.drawRect(100, 100, 400, 400, paint);
+        Paint paint1 = new Paint();
+        paint1.setColor(Color.WHITE);
+        paint1.setStyle(Paint.Style.FILL);
+        canvas.drawRect(200, 200, 300, 300, paint1);
+    }
 
 
     }
