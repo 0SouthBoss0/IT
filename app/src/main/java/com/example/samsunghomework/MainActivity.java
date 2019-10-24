1a5ab201e072ff496f9d00c7f8434b001a59f2e9
Michael <mishumpel@mail.ru>
2019-10-24 18:58:05 +0300

    1.8.8
    graph

--- app/src/main/java/com/example/samsunghomework/MainActivity.java
+++ app/src/main/java/com/example/samsunghomework/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        setContentView(new MyGraph(this));
     }
 }
