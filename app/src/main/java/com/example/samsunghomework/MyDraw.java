6eb629cba7638fc3c5cff0ecb69056f377ccc36d
Michael <mishumpel@mail.ru>
2019-11-01 18:50:14 +0300

    1.9.6
    more logic

--- app/src/main/java/com/example/samsunghomework/MyDraw.java
+++ app/src/main/java/com/example/samsunghomework/MyDraw.java
@@ -1,89 +1,18 @@
 package com.example.samsunghomework;
 
-<<<<<<< HEAD
-
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Bundle;
-
-import android.view.LayoutInflater;
-=======
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
->>>>>>> master
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.Button;
-import android.widget.GridLayout;
 
-<<<<<<< HEAD
-
-public class MyDraw extends Activity implements OnClickListener,
-        OnLongClickListener {
-
-    private int WIDTH = 10;
-    private int HEIGHT = 10;
-
-    private Button[][] cells;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.cells);
-        makeCells();
-
-        generate();
-
-    }
-
-    void generate() {
-
-
-        int num = 1;
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++) {
-                cells[i][j].setText(num + "");
-                num++;
-            }
-
-
-}
-
-    @Override
-    public boolean onLongClick(View v) {
-        //Эту строку нужно удалить
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-
-
-        Button tappedCell = (Button) v;
-
-        //Получаем координтаты нажатой клетки
-        int tappedX = getX(tappedCell);
-        int tappedY = getY(tappedCell);
-        for (int x = 0; x < WIDTH; x++)
-        {
-            cells[tappedY][x].setBackgroundColor(Color.RED);
-        }
-        for (int y = 0; y < WIDTH; y++)
-        {
-            cells[y][tappedX].setBackgroundColor(Color.RED);
-        }
-
-=======
 public class MyDraw extends View {
 
     int N = 50; // количество шариков
-    float[] x  = new float[N];
-    float[] y  = new float[N];
+    float[] x = new float[N];
+    float[] y = new float[N];
     float[] vx = new float[N];
     float[] vy = new float[N];
+
     public MyDraw(Context context) {
         super(context);
         for (int i = 0; i < N; i++) {
@@ -94,62 +23,29 @@ public class MyDraw extends View {
         }
 
     }
-    void add(float[] array , float[] values){
-        for (int i = 0; i < array.length; i++){
+
+    void add(float[] array, float[] values) {
+        for (int i = 0; i < array.length; i++) {
             array[i] += values[i];
         }
     }
+
     Paint paint = new Paint();
 
     @Override
     protected void onDraw(Canvas canvas) {
         // отрисовываем все шарики
+        drawBalls(canvas);
+        // готовим массивы x и у для следующего кадра
+        add(x, vx);
+        add(y, vy);
+        // запрашиваем перерисовку
+        invalidate();
+    }
+
+    private void drawBalls(Canvas canvas) {
         for (int i = 0; i < N; i++) {
             canvas.drawCircle(x[i], y[i], 20, paint);
         }
-        // готовим массивы x и у для следущего кадра
-        for (int i = 0; i < N; i++) {
-            x[i] += vx[i];
-            y[i] += vy[i];
-        }
-        add(x, vx);
-        add(y, vy);
-        //запрашиваем перерисовку
-        invalidate();
-
->>>>>>> master
     }
-
-
-
-    /*
-     * NOT FOR THE BEGINNERS
-     * ==================================================
-     */
-
-    int getX(View v) {
-        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
-    }
-
-    int getY(View v) {
-        return Integer.parseInt(((String) v.getTag()).split(",")[0]);
-    }
-
-    void makeCells() {
-        cells = new Button[HEIGHT][WIDTH];
-        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
-        cellsLayout.removeAllViews();
-        cellsLayout.setColumnCount(HEIGHT);
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++) {
-                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
-                cells[i][j].setOnClickListener(this);
-                cells[i][j].setOnLongClickListener(this);
-                cells[i][j].setTag(i + "," + j);
-                cellsLayout.addView(cells[i][j]);
-            }
-    }
-
 }
