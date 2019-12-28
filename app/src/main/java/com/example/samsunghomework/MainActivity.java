7c5c873855d0cd3ae022767a7d809391a8e18db2
LisaGassy <femida@mail.ru>
2019-12-28 13:47:21 +0300

    shop assistant 0.1
    skeleton of program

--- app/src/main/java/com/example/samsunghomework/MainActivity.java
+++ app/src/main/java/com/example/samsunghomework/MainActivity.java
@@ -1,104 +1,138 @@
 package com.example.samsunghomework;
 
-import android.os.AsyncTask;
+
 import android.os.Bundle;
+
 import android.view.View;
-import android.widget.Button;
-import android.widget.TabHost;
+
+
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    QQ qq = new QQ();
-    int CRACK = 0;
-    int i = 0;
+import java.util.Random;
 
+/////////////////////////////////////////////////////////////////////////////
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txt = findViewById(R.id.txt);
-        final Button button1 = findViewById(R.id.bt1);
-        Button button2 = findViewById(R.id.bt2);
 
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-
-            public void onClick(View view) {
-
-                if (CRACK == 0) {
-                    qq.execute();
-                } else {
-                    i = 0;
-                    CRACK = 0;
-                }
-
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                qq.cancel();
-
-                TextView txt = findViewById(R.id.txt);
-                txt.setText("0");
-            }
-        });
-    }
-
-
-    public void onProgressUpdate(Integer qqq) {
-
-        TextView txt = findViewById(R.id.txt);
-        Integer qqqq = qqq;
-        txt.setText(String.valueOf(qqqq));
 
     }
 
 
-    private class QQ extends AsyncTask<Integer, Integer, Integer> { ///
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+    public void onClick(View view) {
+        TextView q = findViewById(R.id.q);
+        TextView w = findViewById(R.id.w);
+        TextView e = findViewById(R.id.e);
+        TextView r = findViewById(R.id.r);
+        TextView t = findViewById(R.id.t);
+        TextView y = findViewById(R.id.y);
+
+        Random random = new Random();
+
+        int morkov = (random.nextInt(10));
+        int moloko = (random.nextInt(10));
+        int kartofan = (random.nextInt(10));
+        int lyk = (random.nextInt(10));
+        int xleb = (random.nextInt(10));
+        int dirol = (random.nextInt(5));
+        terminal(moloko, morkov, kartofan, lyk, xleb, dirol);
+        morkovH(morkov);
+        molokoH(moloko);
+        kartofanH(kartofan);
+        lykH(lyk);
+        xlebH(xleb);
+        dirolH(dirol);
+        ////////// DEBUGGENO ////////////////
+
+
+    }
+////////////////////////////////////////////////////////////
+
+
+    public void terminal(int morkov, int moloko, int lyk, int kartofan, int xleb, int dirol) {
+        Random random = new Random();
+
+        TextView q = findViewById(R.id.q);
+        TextView w = findViewById(R.id.w);
+        TextView e = findViewById(R.id.e);
+        TextView r = findViewById(R.id.r);
+        TextView t = findViewById(R.id.t);
+        TextView y = findViewById(R.id.y);
+        ////////// DEBUGGENO ////////////////
+
+        int[] array = new int[5];
+        for (int i = 0; i < 5; i++) {
+            array[i] = (random.nextInt(5));
+            if (array[i] == 0) morkovH(i);
+            if (array[i] == 1) molokoH(i);
+            if (array[i] == 2) kartofanH(i);
+            if (array[i] == 3) lykH(i);
+            if (array[i] == 4) xlebH(i);
+            if (array[i] == 5) dirolH(i);
         }
 
+    }
 
-        protected Integer doInBackground(Integer... args) {
-            for (int i = 0; i < 1000000; i++) {
-
-                try {
-                    Thread.sleep(1000); ///задержка
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                peredacha1(i);
-
-            }
-
-
-            return null;
+    private void morkovH(int i, int morkov) {
+        Random random = new Random();
+        int qwertyuiop = 0;
+        while (qwertyuiop == 0) {
+            qwertyuiop = (random.nextInt(3));
         }
 
-        private void peredacha1(Integer i) {
-            Integer qq = i;
-            peredacha2(qq);
+    }
+
+    private void molokoH(int i, int moloko) {
+        Random random = new Random();
+        int qwertyuiop = 0;
+        while (qwertyuiop == 0) {
+            qwertyuiop = (random.nextInt(3));
         }
+
+    }
+
+    private void kartofanH(int i, int kartofan) {
+        Random random = new Random();
+        int qwertyuiop = 0;
+        while (qwertyuiop == 0) {
+            qwertyuiop = (random.nextInt(3));
+        }
+
     }
 
 
-    private void peredacha2(Integer qq) {
-        Integer qqq = qq;
-        onProgressUpdate(qqq);
+    private void lykH(int i, int lyk) {
+        Random random = new Random();
+        int qwertyuiop = 0;
+        while (qwertyuiop == 0) {
+            qwertyuiop = (random.nextInt(3));
+        }
+
+    }
+
+    private void xlebH(int i, int xleb) {
+        Random random = new Random();
+        int qwertyuiop = 0;
+        while (qwertyuiop == 0) {
+            qwertyuiop = (random.nextInt(3));
+        }
+
+    }
+
+    private void dirolH(int i, int dirol) {
+        Random random = new Random();
+        int qwertyuiop = 0;
+        while (qwertyuiop == 0) {
+            qwertyuiop = (random.nextInt(3));
+        }
     }
 
 
 }
+
 
