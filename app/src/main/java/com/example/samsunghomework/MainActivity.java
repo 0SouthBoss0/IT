357ae697bfd15b4d53ed12affa4463b29e6406b8
Michael <mishumpel@mail.ru>
2020-03-27 21:20:25 +0300

    project 0.01
    xml and first floor button

--- app/src/main/java/com/example/samsunghomework/MainActivity.java
+++ app/src/main/java/com/example/samsunghomework/MainActivity.java
@@ -3,23 +3,51 @@ package com.example.samsunghomework;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.example.samsunghomework.R.id.*;
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-
-
+public class MainActivity extends AppCompatActivity {
+    Button winter;
+    Button middle;
+    Button summer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new Draw(this));
+        setContentView(R.layout.activity_main);
+        winter = (Button) findViewById(R.id.winter);
+        middle = (Button) findViewById(Middle);
+        summer = (Button) findViewById(Summer);
+
     }
 
-    @Override
-    public void onClick(View v) {
+    int FIRST = 0;
+
+    public void Winter(View view) {
+        FIRST = 1;
+        toSecond();
+
+    }
+
+
+    public void Middle(View view) {
+        FIRST = 1;
+        toSecond();
+    }
+
+    public void Summer(View view) {
+        FIRST = 1;
+        toSecond();
+    }
+
+    public void toSecond() {
+
     }
 }
+
+
+
