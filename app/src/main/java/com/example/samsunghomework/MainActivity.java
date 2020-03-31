bd7f3a6bf578399e9ec40a17b4ddcf198a22e669
Michael <mishumpel@mail.ru>
2020-03-31 19:46:51 +0300

    0.04 GALKA Animation

--- app/src/main/java/com/example/samsunghomework/MainActivity.java
+++ app/src/main/java/com/example/samsunghomework/MainActivity.java
@@ -26,28 +26,57 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    int FIRST = 0;
+
+    int galka = 0;
+    int curr = 0;
 
     public void Winter(View view) {
-        FIRST = 1;
-        toSecond();
+        if (galka == 0) winter.setImageResource(R.drawable.galka);
+        else if (curr == 2) {
+            middle.setImageResource(R.drawable.listok);
+            winter.setImageResource(R.drawable.galka);
+        }
+        if (curr == 3) {
+            summer.setImageResource(R.drawable.unnamed);
+            winter.setImageResource(R.drawable.galka);
+        }
+        galka = 1;
+        curr = 1;
 
     }
 
 
     public void Middle(View view) {
-        FIRST = 1;
-        toSecond();
+        if (galka == 0) middle.setImageResource(R.drawable.galka);
+        else if (curr == 1) {
+            winter.setImageResource(R.drawable.snejinka);
+            middle.setImageResource(R.drawable.galka);
+        }
+        if (curr == 3) {
+            summer.setImageResource(R.drawable.unnamed);
+            middle.setImageResource(R.drawable.galka);
+        }
+        galka = 1;
+        curr = 2;
     }
 
     public void Summer(View view) {
-        FIRST = 1;
-        toSecond();
+        if (galka == 0) summer.setImageResource(R.drawable.galka);
+        else if (curr == 1) {
+            winter.setImageResource(R.drawable.snejinka);
+            summer.setImageResource(R.drawable.galka);
+        }
+
+        if (curr == 2) {
+            middle.setImageResource(R.drawable.listok);
+            summer.setImageResource(R.drawable.galka);
+
+        }
+        galka = 1;
+        curr = 3;
     }
 
-    public void toSecond() {
 
-    }
 }
 
 
