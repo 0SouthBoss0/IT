74b4ee34d6ec3aa42b1b3618d0ac7340628d659a
Michael <mishumpel@mail.ru>
2020-04-05 12:42:43 +0300

    CRASH
       java.lang.RuntimeException: Canvas: trying to draw too large(139197440bytes) bitmap.

--- app/src/main/java/com/example/samsunghomework/MainActivity.java
+++ app/src/main/java/com/example/samsunghomework/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     ImageButton winter;
     ImageButton middle;
     ImageButton summer;
+    ImageButton man;
+    ImageButton child;
+    ImageButton woman;
+    LinearLayout mainlayout;
+
+
     ImageButton three;
     ImageButton seven;
     ImageButton fourteen;
@@ -26,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
         winter = (ImageButton) findViewById(R.id.winter);
         middle = (ImageButton) findViewById(Middle);
         summer = (ImageButton) findViewById(Summer);
-        three = (ImageButton) findViewById(R.id.three);
-        seven = (ImageButton) findViewById(R.id.seven);
-        fourteen = (ImageButton) findViewById(R.id.fourteen);
+        mainlayout = (LinearLayout) findViewById(R.id.mainlayout);
+        man = (ImageButton) findViewById(R.id.man);
+        child = (ImageButton) findViewById(R.id.child);
+        woman = (ImageButton) findViewById(R.id.woman);
+
     }
 
 
@@ -36,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
     int curr = 0;
 
     public void Winter(View view) {
+        //   mainlayout.removeAllViews();
         if (galka == 0) winter.setImageResource(R.drawable.galka);
+
         else if (curr == 2) {
             middle.setImageResource(R.drawable.listok);
             winter.setImageResource(R.drawable.galka);
@@ -47,7 +59,16 @@ public class MainActivity extends AppCompatActivity {
         }
         galka = 1;
         curr = 1;
+        //  mainlayout.addView(new ImageButton(this));
+       /*
+        ImageButton imageButton = new ImageButton(MainActivity.this);
+        imageButton.setImageResource(R.drawable.galka);
+        LinearLayout.LayoutParams imageViewLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
+        imageButton.setLayoutParams(imageViewLayoutParams);
+
+        mainlayout.addView(imageButton);
+*/
     }
 
 
@@ -84,53 +105,58 @@ public class MainActivity extends AppCompatActivity {
     int galka2 = 0;
     int curr2 = 0;
 
-    public void three(View view) {
-        if (galka2 == 0) three.setImageResource(R.drawable.galka);
+
+    public void Man(View view) {
+        if (galka2 == 0) man.setImageResource(R.drawable.galka);
         else if (curr2 == 2) {
-            seven.setImageResource(R.drawable.seven);
-            three.setImageResource(R.drawable.galka);
+            child.setImageResource(R.drawable.child);
+            man.setImageResource(R.drawable.galka);
         }
         if (curr2 == 3) {
-            fourteen.setImageResource(R.drawable.fourteen);
-            three.setImageResource(R.drawable.galka);
+            woman.setImageResource(R.drawable.woman);
+            man.setImageResource(R.drawable.galka);
         }
         galka2 = 1;
         curr2 = 1;
 
     }
 
-    public void seven(View view) {
-        if (galka2 == 0) seven.setImageResource(R.drawable.galka);
+
+    public void Child(View view) {
+        if (galka2 == 0) child.setImageResource(R.drawable.galka);
         else if (curr2 == 1) {
-            three.setImageResource(R.drawable.three);
-            seven.setImageResource(R.drawable.galka);
+            man.setImageResource(R.drawable.man);
+            child.setImageResource(R.drawable.galka);
         }
         if (curr2 == 3) {
-            fourteen.setImageResource(R.drawable.fourteen);
-            seven.setImageResource(R.drawable.galka);
+            woman.setImageResource(R.drawable.woman);
+            child.setImageResource(R.drawable.galka);
         }
         galka2 = 1;
         curr2 = 2;
 
     }
 
-    public void fourteen(View view) {
-        if (galka2 == 0) fourteen.setImageResource(R.drawable.galka);
+
+    public void Woman(View view) {
+        if (galka2 == 0) woman.setImageResource(R.drawable.galka);
         else if (curr2 == 1) {
-            three.setImageResource(R.drawable.three);
-            fourteen.setImageResource(R.drawable.galka);
+            man.setImageResource(R.drawable.man);
+            woman.setImageResource(R.drawable.galka);
         }
 
         if (curr2 == 2) {
-            seven.setImageResource(R.drawable.seven);
-            fourteen.setImageResource(R.drawable.galka);
+            child.setImageResource(R.drawable.child);
+            woman.setImageResource(R.drawable.galka);
 
         }
         galka2 = 1;
         curr2 = 3;
 
     }
+
 }
+
 
 
 
