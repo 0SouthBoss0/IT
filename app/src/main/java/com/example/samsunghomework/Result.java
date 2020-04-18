e08bfb032fdca6ba45cdfa691bb4c36a794b699f
Michael <mishumpel@mail.ru>
2020-04-18 20:27:40 +0300

    0.6 skeleton of >10DAYS

--- app/src/main/java/com/example/samsunghomework/Result.java
+++ app/src/main/java/com/example/samsunghomework/Result.java
@@ -3,6 +3,7 @@ package com.example.samsunghomework;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,13 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Result extends AppCompatActivity {
 
     CheckBox trysi;
-    CheckBox rubaha;
-    CheckBox footbolka;
-    CheckBox shorti;
     CheckBox noski;
-    CheckBox bruki;
-    CheckBox kofta;
-    CheckBox obuv;
     CheckBox dop1;
     CheckBox dop2;
     CheckBox dop3;
@@ -25,6 +20,11 @@ public class Result extends AppCompatActivity {
     CheckBox dop5;
     CheckBox dop6;
     CheckBox dop7;
+    CheckBox dop8;
+    CheckBox dop9;
+    CheckBox dop10;
+
+
     TextView text;
     MainActivity mainActivity = new MainActivity();
 
@@ -32,24 +32,20 @@ public class Result extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
         trysi = (CheckBox) findViewById(R.id.trusi);
-        rubaha = (CheckBox) findViewById(R.id.rubaha);
-        footbolka = (CheckBox) findViewById(R.id.footbalka);
-        shorti = (CheckBox) findViewById(R.id.shorti);
         noski = (CheckBox) findViewById(R.id.nocki);
-        bruki = (CheckBox) findViewById(R.id.bruki);
-        kofta = (CheckBox) findViewById(R.id.kofta);
-        obuv = (CheckBox) findViewById(R.id.obuv);
-
-        dop1 = (CheckBox) findViewById(R.id.dop1);
-        dop2 = (CheckBox) findViewById(R.id.dop2);
-        dop3 = (CheckBox) findViewById(R.id.dop3);
-        dop4 = (CheckBox) findViewById(R.id.dop4);
-        dop5 = (CheckBox) findViewById(R.id.dop5);
-        dop6 = (CheckBox) findViewById(R.id.dop6);
-        dop7 = (CheckBox) findViewById(R.id.dop7);
-
+        dop1 = (CheckBox) findViewById(R.id.rubaha);
+        dop2 = (CheckBox) findViewById(R.id.footbalka);
+        dop3 = (CheckBox) findViewById(R.id.bruki);
+        dop4 = (CheckBox) findViewById(R.id.kofta);
+        dop5 = (CheckBox) findViewById(R.id.obuv);
+        dop6 = (CheckBox) findViewById(R.id.dop1);
+        dop7 = (CheckBox) findViewById(R.id.dop2);
+        dop8 = (CheckBox) findViewById(R.id.dop3);
+        dop9 = (CheckBox) findViewById(R.id.dop4);
+        dop10 = (CheckBox) findViewById(R.id.dop5);
 
         text = (TextView) findViewById(R.id.WELCOME);
+
         text.setText("Ваш чек-лист: ");
 
         int curr1 = getIntent().getExtras().getInt("curr1");
@@ -58,32 +54,82 @@ public class Result extends AppCompatActivity {
         int DAY = getIntent().getExtras().getInt("DAY");
 
 
-        if (DAY > 10) {
-            noski.setText("Носки: 10 шт.");
-            footbolka.setText("Футболка: 8 шт.");
-            trysi.setText("Нижнее бельё: " + "8" + " шт.");
-            rubaha.setText("Рубашка: 4 шт.");
-            kofta.setText("Кофта: 2 шт.");
-
-
-            obuv.setText("Обувь: 2 шт.");
-            shorti.setText("Шорты: 2 шт");
-            bruki.setText("Брюки: 3 шт.");
-
-        } else {
-
-            trysi.setText("Нижнее бельё: " + DAY + " шт.");
-            rubaha.setText("Рубашка: " + (int) Math.round(DAY / 2) + " шт.");
-            footbolka.setText("Футболка: " + (int) Math.round((DAY / 2) + 1) + " шт.");
+        if (DAY <= 3) {
+            trysi.setText("Нижнее белье: " + DAY + " шт.");
             noski.setText("Носки: " + DAY + " шт.");
-            kofta.setText("Кофта " + (int) Math.round(DAY / 3) + " шт.");
-
-            obuv.setText("Обувь: 1 шт.");
-            shorti.setText("Шорты: 1 шт.");
-            bruki.setText("Брюки " + (int) Math.round(DAY / 2) + " шт");
 
 
         }
+        if (DAY > 3 && DAY <= 10) { //СТИРКА
+            trysi.setText("Нижнее белье " + (int) Math.round(DAY / 2) + " шт.");
+            noski.setText("Носки " + (int) Math.round(DAY / 2) + " шт.");
+        }
+
+
+        if (DAY > 10) {
+            trysi.setText("Нижнее белье: 7 шт.");
+            noski.setText("Носки: 7 пар");
+            if (curr1 == 1) {
+                dop1.setText("Теплая непромокаемая куртка: 1 шт.");
+                dop2.setText("Шарф, перчатки: 2 пары");
+                dop3.setText("Термобелье: 3 шт.");
+                dop4.setText("Теплые штаны: 2 шт.");
+                dop5.setText("Теплая обувь: 2 пары");
+            }
+            if (curr2 == 1) {
+                dop6.setText("Рубашка: 3 шт.");
+                dop7.setText("Футболка: 4 шт.");
+                dop8.setText("Кофта/джемпер: 3 шт.");
+                dop9.setText("Тапочки в дом: 1 пара");
+                dop10.setText("Брюки+ремень: 2 шт.");
+
+
+            }
+
+            if (curr3 == 1) {
+                //туризм
+            }
+
+
+            if (curr1 == 2) {
+                dop1.setText("Непромокаемая куртка с капюшоном: 1 шт.");
+                dop2.setText("Шарф, перчатки: 1 пара");
+                dop3.setText("Теплые штаны: 1 шт.");
+                dop4.setText("Теплая обувь: 2 пары");
+                dop5.setText("Зонт: 1 шт.");
+            }
+            if (curr2 == 2) {
+                dop6.setText("Футболка с длинным рукавом: 4 шт.");
+                dop7.setText("Кофта/джемпер: 3 шт.");
+                dop8.setText("Слюнявчик: 1 шт.");
+                dop9.setText("Тапочки в дом: 1 пара");
+                dop10.setText("Подгузники: 1 упаковка");
+            }
+            if (curr3 == 2) {
+                //командировка
+            }
+            if (curr1 == 3) {
+                dop1.setText("Шорты: 3 шт.");
+                dop2.setText("Купальный костюм: 1 шт.");
+                dop3.setText("Сланцы: 1 пара");
+                dop4.setText("Удобные кроссовки: 2 пары");
+                dop5.setText("Солнцезащитные очки: 1 шт");
+            }
+
+            if (curr2 == 3) {
+                dop6.setText("Блузка: 3 шт.");
+                dop7.setText("Юбка: 2 шт.");
+                dop8.setText("Тапочки в дом: 1 пара");
+                dop9.setText("Платье: 3 шт.");
+                dop10.setText("Кофта/джемпер: 2 шт.");
+            }
+            if (curr3 == 3) {
+                //пляж
+            }
+        }
+
+
     }
 }
+
 
